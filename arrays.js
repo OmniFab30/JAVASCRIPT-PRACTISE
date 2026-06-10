319df4134b7de3a3527313917460416aec5c62b7
OmniFab <omnifabrakotondrajaona@gmail.com>
2026-06-10 21:56:07 +0300

    Add new solutions

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ firstname.shift()
 console.log(firstname)
 
 /*Exercice 05: calcul sum*/
-const notes = [10,19,14,9,12]
+const notes = [10,19,14,9,12,10]
 let sum = 0
 for(let note of notes) sum += note
 console.log(`Sum: ${sum}`)
@@ -40,7 +40,87 @@ console.log(max)
 for(let note of notes)(min > note) ? min = note : min
 console.log(min)
 
-/*Exercice 08: search an element in notes array */
-//using no methods
+/*Exercice 08: count pair number in notes array */
+let countPairNote = 0
+for(let note of notes)(note % 2 === 0) ? countPairNote++ : countPairNote
+console.log(`notes array contains ${countPairNote} pair numbers`)
 
+/*Exercice 09: search an element no using  methods*/
+const search = 10
+for(let note of notes) {
+    if(note === search){
+        console.log(`${search} is in the list`)
+        break
+    }
+    else{
+        console.log(`${search} isn't in the list`)
+        break
+    }
+}
+
+/*Exercice 10: get the index of element no using  methods*/
+//basic
+let index = 0
+ const numberSearch = 12
+for(let i = 0; i < notes.length; i++){
+    if(numberSearch === notes[i]){
+        index = i
+        console.log(`element: ${numberSearch} index : ${index}`)
+    } 
+}
+
+//using forEach
+notes.forEach((element, index) => {
+    if (element ===  numberSearch) console.log(`element: ${element} | index: ${index}`) 
+});
+
+/*Exercice 11: count an element in array*/
+let countNumber = 0
+const countElement = notes.forEach(element => {
+    if(element === numberSearch) countNumber++
+});
+console.log(`${numberSearch}: There are ${countNumber} in the list`)
+
+/*Exercice 12: reverse an array no using reverse() method*/
+const n = [0,1,2,3,4,5]
+const nReversed = []
+for(let i = n.length - 1; i > -1;  i--) nReversed.push(i)
+console.log(nReversed)
+
+// reverse array on the place
+let left = 0
+let right = n.length - 1
+
+while(left < right){
+    //reverse element
+    let tmp = n[left]
+    n[left] = n[right]
+    n[right] = tmp
+
+    left ++
+    right--
+}
+console.log(n)
+
+/*Exercice 13: create new array contains only a pair number*/
+const pairArray = []
+for(let number in n) if(number % 2 === 0) pairArray.push(number)
+console.log(pairArray)
+
+ /*Exercice 14: multiply each element by 2 no using method*/
+const arrayMultiplied = []
+for(let i in n) arrayMultiplied.push(i * 2)
+console.log(arrayMultiplied)
+
+/*Exercice 15: delete duplicates value*/
+const duplicateArray = [4,4,5,5,8,9,1,2,4,5]
+const uniqueValue = []
+for(let number of duplicateArray) (!uniqueValue.includes(number)) && uniqueValue.push(number)
+console.log(uniqueValue)
+
+/*Exercice 16: fusion two arrays*/
+const firstArray = [1,2,3,4,5]
+const secondArray = [6,7,8]
+for(let n of secondArray) firstArray.push(n)
+console.log(firstArray)
 
